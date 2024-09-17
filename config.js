20d9398f35ba1538e8d090f216d416b170c05b33
AlexPerez <alexperez123@gmail.com>
2024-09-17 18:51:20 -0400

    Task 3 complete

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   API_URL:
-    "https://cnt-d4f7dc93-fbaf-4ffb-b1a1-6693092f0db3.containerhub.tripleten-services.com",
+    "https://cnt-4098148b-ef73-42c6-8999-af42edac3b87.containerhub.tripleten-services.com",
 };
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,18 +1,26 @@
 // eslint-disable-next-line no-undef
-const config = require('../config');
+const config = require("../config");
 
-const requestBody = {}
+const requestBody = {
+  price: 175,
+};
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
-			method: 'PUT',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
-	} catch (error) {
-		console.error(error);
-	}
+test("Status code should be 200 and update product price", async () => {
+  try {
+    const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const actualStatusCode = response.status;
+    const data = await response.json();
+  } catch (error) {
+    console.error(error);
+
+    expect(actualStatusCode).toBe(200);
+    expect(data.ok).toBe(true);
+  }
 });
