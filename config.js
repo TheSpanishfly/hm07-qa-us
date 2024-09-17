636228a0921720e5015bc748acb2925709857df0
AlexPerez <alexperez123@gmail.com>
2024-09-17 14:05:53 -0400

    Working on Task 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   API_URL:
-    "https://cnt-8c2c291d-96af-4fae-9272-b7801d9035c5.containerhub.tripleten-services.com",
+    "https://cnt-65d38604-c53e-4a5d-8d0f-2419dde37c91.containerhub.tripleten-services.com",
 };
--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -1,19 +1,31 @@
 // eslint-disable-next-line no-undef
 const config = require("../config");
+const fetch = require("node-fetch");
 
 const requestBody = {
-  // put your body here
+  products: [
+    { id: 5, quantity: 1 },
+    { id: 4, quantity: 5 },
+  ],
 };
 
-test("", async () => {
+test("Return status code of 200 and check if products are available in warehouses", async () => {
+  let actualStatusCode;
+  let data;
+
   try {
-    const response = await fetch(`${config.API_URL}/your/endpoint`, {
+    const response = await fetch(`${config.API_URL}//api/v1/warehouses/check`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(requestBody),
+      body: JSON.stringify(requestBody),
     });
+
+    actualStatusCode = response.status;
+    expect(actualStatusCode).toBe(200);
+    data = await response.json();
   } catch (error) {
     console.error(error);
   }
