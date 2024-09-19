e7929c3ba24715285b713ea0492f8ffe73d217e3
AlexPerez <alexperez123@gmail.com>
2024-09-18 21:49:07 -0400

    First Update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   API_URL:
-    "https://cnt-ba604662-948e-48bb-b944-6d463d73d56e.containerhub.tripleten-services.com",
+    "https://cnt-0cba5eef-6414-4d60-b9da-d2f70c33c657.containerhub.tripleten-services.com",
 };
